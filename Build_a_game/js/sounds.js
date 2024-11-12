0755de019c780abf24f6e43587d0f7f376998d0b
JimPolder <302101233@student.rocmondriaan.nl>
2024-11-13 00:09:34 +0100

    add audio

---
+++ Build_a_game/js/sounds.js
@@ -0,0 +1,13 @@
+//Navbar sounds
+
+//home
+function Homesound(){
+    let soundvar = new Audio("../audio/SFX/Home.wav"); 
+    soundvar.play();
+}
+
+//minesweeper
+
+//pkmn battle sim
+
+//4 op een rij
