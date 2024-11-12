17288342c797d061618821e4bf11264d55ebb0aa
JimPolder <302101233@student.rocmondriaan.nl>
2024-11-12 23:50:48 +0100

    add home preview + other html files

--- Build_a_game/js/main.js
+++ Build_a_game/js/main.js
@@ -1,26 +1,42 @@
+const homebtn = document.querySelector(".Home")
 const mnswpbtn = document.querySelector(".Minesweeper")
 const pkmnbtn = document.querySelector(".Pokémon_battle_sim")
 const rijbutton = document.querySelector(".op_een_rij")
 
 const preview = document.querySelector(".preview")
 
+const previewhome = `HomeIcon.png`
 const previewmnswp = `Minesweeper.png`
 const previewpkmn = `pkmnbattlesim.png`
 const preview4r =  `4r.png`
 
 //mouse hover functions
+
+//home
+homebtn.addEventListener("mouseover", function(){
+    previewfnOn(previewhome)
+})
+homebtn.addEventListener("mouseout", function(){
+    previewfnOff(previewhome)
+})
+
+//minesweeper
 mnswpbtn.addEventListener("mouseover", function(){
     previewfnOn(previewmnswp)
 })
 mnswpbtn.addEventListener("mouseout", function(){
     previewfnOff(previewmnswp)
 })
+
+//pokemon battle sim
 pkmnbtn.addEventListener("mouseover", function(){
      previewfnOn(previewpkmn)
  })
 pkmnbtn.addEventListener("mouseout", function(){
      previewfnOff(previewpkmn)
  })
+
+//4 op een rij
 rijbutton.addEventListener("mouseover", function(){
     previewfnOn(preview4r)
 })
@@ -28,6 +44,9 @@ rijbutton.addEventListener("mouseout", function(){
     previewfnOff(preview4r)
 })
 
+//Preview functions
+
+//on
 function previewfnOn(game){
     preview.innerHTML = `<img src="./img/${game}" class = "temppreview" width="90" height="90">`
     document.querySelector(".preview").style.paddingLeft  = "50px"
@@ -35,6 +54,8 @@ function previewfnOn(game){
     document.querySelector(".temppreview").style.border = "medium solid rgb(255, 0, 0)";
 
 }
+
+//off
 function previewfnOff(game){
     preview.innerHTML = ` `
 }
